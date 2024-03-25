32bd34c90781dc1bf131feb17b1f2af9060b49e9
danieledisco <daniele.disco.1965@gmail.com>
2024-03-26 00:52:15 +0100

    Aggiunti alcuni elementi dopo aver risolto il problema di cache

--- app/Layout/main.php
+++ app/Layout/main.php
@@ -4,12 +4,14 @@
                 <div class="row">
                     <?php foreach ($products as $product) : ?>
                         <div class="col-12 col-sm-6 col-md-4">
-                            <h5>Categoria: <i class="<?php echo $product->icons; ?>"></i></h5>
+                            <h4>
+                                Categoria: <i class="<?php echo $product->icons; ?>"></i>
+                            </h4>
                             <h5>Descrizione: <?php echo $product->description; ?> </h5>
                             <h5>Prezzo: <?php echo $product->price; ?> </h5>
                             <h5>Tipo di articolo: <?php echo $product->kind; ?> </h5>
                             <div class="imgContainer">
-                                <img src="<?php echo $product->img; ?>" , width=100, alt="">
+                                <img src="<?php echo $product->img; ?>" , alt="">
                             </div>
                         </div>
                     <?php endforeach ?>
